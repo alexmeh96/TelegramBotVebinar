fdb1cf264ae8e85924b7ceb962a845d1caadec6c
alexmeh <alex.meh96@mail.ru>
2020-06-05 17:52:38 +0300

    "done3"

---
+++ src/main/java/org/itmo/Components/service/BackgroundBot.java
@@ -0,0 +1,41 @@
+package org.itmo.Components.service;
+
+import org.itmo.Components.model.TelegramUsers;
+import org.itmo.Components.model.User;
+import org.itmo.MainTelegramBot;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class BackgroundBot {
+
+    @Autowired
+    TelegramUsers telegramUsers;
+    @Autowired
+    MainTelegramBot mainTelegramBot;
+
+    @Scheduled(cron="*/60 * * * * *", zone="Europe/Moscow")
+    public void doScheduledWork() {
+        System.out.println("method");
+
+        for (String num : telegramUsers.getMapDate().keySet()){
+            for (User user : telegramUsers.getUserMap().values()){
+                if (!user.getSendHW().contains(num)){
+
+                    SendMessage sendMessage = new SendMessage();
+                    sendMessage.setChatId(user.getChatId());
+                    sendMessage.setText("Вы ещё не сделали домашнее задание " + num + "!");
+                    try {
+                        mainTelegramBot.execute(sendMessage);
+                    } catch (TelegramApiException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ src/main/java/org/itmo/config/BotProperty.java
@@ -0,0 +1,28 @@
+package org.itmo.config;
+
+public class BotProperty {
+    //колонки новой таблицы
+    public static final byte SHEET_NAME_COL=0;
+    public static final byte SHEET_EMAIL_COL=1;
+    public static final byte SHEET_PASS_COL=2;
+    public static final byte SHEET_PHONE_COL=3;
+    public static final byte SHEET_USERNAME_COL=4;
+    public static final byte SHEET_ROLE_COL=5;
+    public static final byte SHEET_CASH_COL=6;
+    public static final byte SHEET_TARIF_COL=7;
+
+    public static final String TOKEN = "<Token>";
+    public static final String PATH = "/home/alex/work/java/Projects/TelegramBotVebinar/src/main";
+
+    public static final String ADMIN = "@MarkStav";
+    public static final String SPIKER = "@GPlesovskikh";
+
+    public static final String SPREADSHEET_ID = "1wOOgK2KK6OE7tmLPsJR-_Jt_sBVfCtD0Qk-n1CqZpbc";
+
+//    public static final Long TIME_SAVE_QUESTION = 172_800_000L;  //48 часов
+    public static final Long TIME_QUESTION = 120_000L;
+    public static final Long TIME_HW = 120_000L;
+
+
+
+}
